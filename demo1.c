c2f735c56b1638eecf64423468768beec76252e2
Your Name <1142918693@qq.com>
2021-05-20 21:24:52 +0800

    demo1.c--1.3.2版本；商城开发中，master出现bug，及时修复

--- demo1.c
+++ demo1.c
@@ -1,10 +1,14 @@
 
 #include <stdio.h>
+void XiuFuBUG(void){
+  printf("ÐÞ¸´BUG");
+}
 void QiChuang(void){
 
   printf("ÔçÉÏÆð´²\r\n");
 }
 int main(){
+  XiuFuBUG();
   QiChuang();
   printf("hello world\r\n");
   system("pause");
