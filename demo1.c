e071dc7069f172b4d17693adde6bb63b9372ab65
Your Name <1142918693@qq.com>
2021-05-20 21:17:22 +0800

    demo1.--1.3.1版本；shangchengkaifa分支上的版本，具有商城函数

--- demo1.c
+++ demo1.c
@@ -1,10 +1,14 @@
 
 #include <stdio.h>
+void ShangCheng(void){
+  printf("小王网上商城开业了\r\n");
+}
 void QiChuang(void){
 
   printf("早上起床\r\n");
 }
 int main(){
+  ShangCheng();
   QiChuang();
   printf("hello world\r\n");
   system("pause");
